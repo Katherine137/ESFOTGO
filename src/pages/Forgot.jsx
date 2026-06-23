f3a5302df20ce0c2777742c48342705ac30c3446
Katherine7713 <katherine.sailema@epn.edu.ec>
2026-06-23 12:04:15 -0500

    Estilos

--- src/pages/Forgot.jsx
+++ src/pages/Forgot.jsx
@@ -4,6 +4,8 @@ import { useFetch } from "../hooks/useFetch";
 import { useForm } from "react-hook-form";
 import Fondo from "../assets/fondo.png";
 
+
+
 export const Forgot = () => {
     const { register, handleSubmit, formState: { errors }} = useForm()
     const fetchDataBackend = useFetch()
@@ -12,47 +14,47 @@ export const Forgot = () => {
         const url = `${import.meta.env.VITE_BACKEND_URL}/recuperarpassword`
         await fetchDataBackend(url, dataForm, "POST")
     }
+
     return (
-        <div className="flex flex-col sm:flex-row h-screen">
-
-            <ToastContainer/>
-
-            <div className="min-h-screen flex flex-col md:flex-row bg-cover bg-center bg-no-repeat"
-                style={{ backgroundImage: `url(${Fondo})` }}
-            >
+        <div 
+            className="min-h-screen relative flex items-center justify-center bg-slate-950 bg-cover bg-center bg-no-repeat"
+            style={{ backgroundImage: `url(${Fondo})` }}
+        >
             
-                <div className="w-full max-w-sm p-8 bg-white rounded-lg shadow-lg">
-                    <h1 className="text-3xl font-semibold text-center text-slate-500">¡Olvidaste la contraseña!</h1>
-                    <br />
+            <div className="absolute inset-0 bg-black/50 z-0"></div>
 
-                    <form onSubmit={handleSubmit(sendMail)}>
-                        <div className="mb-3">
-                            <label className="block text-sm font-semibold mb-1">Correo electrónico</label>
-                            <input
-                                type="email"
-                                placeholder="Ingresa tu correo"
-                                className="w-full rounded-md border border-blue-500 focus:ring-1 px-2 py-1 text-neutral-950"
-                                {...register("email", { required: "El correo electrónico es obligatorio" })}
-                            />
-                            {errors.email && <p className="text-red-900">{errors.email.message}</p>}
-                        </div>
+            <ToastContainer />
+            
+            <div className="relative z-10 w-full max-w-sm p-8 bg-white rounded-lg shadow-lg">
+                <h1 className="text-3xl font-semibold text-center text-slate-500">¡Olvidaste la contraseña!</h1>
+                <br />
 
-                        <div className="mb-3">
-                        <button className="block w-full py-2 text-center bg-red-900 text-white rounded-xl hover:bg-gray-900 duration-300">
-                        Enviar correo</button>
-                        </div>
-                        
-                    </form>
-
-                    <div className="mt-3 flex justify-between text-sm">
-                        <p className="text-neutral-950 font-medium">¿Ya tiene una cuenta?</p>
-                        <Link to="/Login" className="block w-24 py-2 text-center bg-red-900 text-white rounded-xl hover:bg-gray-900 duration-300"> Iniciar sesión </Link>
+                <form onSubmit={handleSubmit(sendMail)}>
+                    <div className="mb-3">
+                        <label className="block text-sm font-semibold mb-1">Correo electrónico</label>
+                        <input
+                            type="email"
+                            placeholder="Ingresa tu correo"
+                            className="w-full rounded-md border border-blue-500 px-2 py-2 text-black"
+                            {...register("email", { required: "El correo es obligatorio" })}
+                        />
+                        {errors.email && <p className="text-red-600 text-xs mt-1">{errors.email.message}</p>}
                     </div>
 
+                    <div className="mb-3">
+                        <button className="block w-full py-2 text-center bg-red-900 text-white rounded-xl hover:bg-gray-900 duration-300">
+                            Enviar correo
+                        </button>
+                    </div>
+                </form>
+
+                <div className="mt-3 flex justify-between items-center text-sm">
+                    <p className="text-gray-600 font-medium">¿Ya tienes cuenta?</p>
+                    <Link to="/Login" className="px-4 py-2 text-center bg-gray-600 text-white rounded-xl hover:bg-gray-900 duration-300"> 
+                        Iniciar sesión 
+                    </Link>
                 </div>
             </div>
-
-            <div className="hidden sm:block sm:w-1/2 bg-[url('/buho.png')] bg-cover bg-center"></div>
         </div>
     )
 }
